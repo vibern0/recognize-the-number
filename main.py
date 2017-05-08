676393e3f7ca326b4582991f48775433330e3f9e
Bernardo Vieira <bernardo5@live.com.pt>
2017-05-08 21:30:17 +0100

    first taining model :tada:

---
+++ main.py
@@ -0,0 +1,43 @@
+import tensorflow as tf
+
+T, F = 1., 0.
+
+train_in = [
+ [T, T],
+ [T, F],
+ [F, T],
+ [F, F],
+]
+
+train_out = [
+ [F],
+ [T],
+ [T],
+ [F],
+]
+
+w1 = tf.Variable(tf.random_normal([2, 2]), name='w1')
+b1 = tf.Variable(tf.zeros([2]), name='b1')
+
+w2 = tf.Variable(tf.random_normal([2, 1]), name='w2')
+b2 = tf.Variable(tf.zeros([1]), name='b2')
+
+out1 = tf.tanh(tf.add(tf.matmul(train_in, w1), b1))
+out2 = tf.tanh(tf.add(tf.matmul(out1, w2), b2))
+
+error = tf.subtract(train_out, out2)
+mse = tf.reduce_mean(tf.square(error))
+
+train = tf.train.GradientDescentOptimizer(0.01).minimize(mse)
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+err, target = 1, 0.01
+epoch, max_epochs = 0, 10000
+
+while err > target and epoch < max_epochs:
+   epoch += 1
+   err, _ = sess.run([mse, train])
+
+print('epoch:', epoch, 'mse:', err)
