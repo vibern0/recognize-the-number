ab01e0d588725fe0d12efa6c6be03fa3812332c5
Bernardo Vieira <bernardo5@live.com.pt>
2017-05-08 22:58:57 +0100

    repeat 35 times

--- main.py
+++ main.py
@@ -16,45 +16,52 @@ train_out = [
  [F],
 ]
 
-w1 = tf.Variable(tf.random_normal([2, 2]), name='w1')
-b1 = tf.Variable(tf.zeros([2]), name='b1')
+# test training and result 35 times
+for x in range(0, 35):
 
-w2 = tf.Variable(tf.random_normal([2, 1]), name='w2')
-b2 = tf.Variable(tf.zeros([1]), name='b2')
+    w1 = tf.Variable(tf.random_normal([2, 2]), name='w1')
+    b1 = tf.Variable(tf.zeros([2]), name='b1')
 
-out1 = tf.tanh(tf.add(tf.matmul(train_in, w1), b1))
-out2 = tf.tanh(tf.add(tf.matmul(out1, w2), b2))
+    w2 = tf.Variable(tf.random_normal([2, 1]), name='w2')
+    b2 = tf.Variable(tf.zeros([1]), name='b2')
 
-error = tf.subtract(train_out, out2)
-mse = tf.reduce_mean(tf.square(error))
+    # activation functions
+    # see more https://www.tensorflow.org/api_guides/python/nn
+    out1 = tf.tanh(tf.add(tf.matmul(train_in, w1), b1))
+    out2 = tf.tanh(tf.add(tf.matmul(out1, w2), b2))
 
-train = tf.train.GradientDescentOptimizer(0.01).minimize(mse)
+    error = tf.subtract(train_out, out2)
+    mse = tf.reduce_mean(tf.square(error))
 
-sess = tf.Session()
-sess.run(tf.global_variables_initializer())
+    # backpropagation method
+    # see more https://www.tensorflow.org/api_guides/python/train
+    train = tf.train.GradientDescentOptimizer(0.01).minimize(mse)
 
-err, target = 1, 0.01
-epoch, max_epochs = 0, 10000
+    sess = tf.Session()
+    sess.run(tf.global_variables_initializer())
 
-while err > target and epoch < max_epochs:
-   epoch += 1
-   err, _ = sess.run([mse, train])
+    err, target = 1, 0.01
+    epoch, max_epochs = 0, 10000
 
-print('epoch:', epoch, 'mse:', err)
+    while err > target and epoch < max_epochs:
+       epoch += 1
+       err, _ = sess.run([mse, train])
 
+    print('epoch:', epoch, 'mse:', err)
 
-t_train_in = [
-    [T, T],
-    [T, F],
-    [F, T],
-    [F, F],
-]
-t_out1 = tf.tanh(tf.add(tf.matmul(t_train_in, w1), b1))
-t_out2 = tf.tanh(tf.add(tf.matmul(t_out1, w2), b2))
-t_result = sess.run([t_out2])
-print('res', t_result)
+    # test results
+    t_train_in = [
+        [T, T],
+        [T, F],
+        [F, T],
+        [F, F],
+    ]
+    t_out1 = tf.tanh(tf.add(tf.matmul(t_train_in, w1), b1))
+    t_out2 = tf.tanh(tf.add(tf.matmul(t_out1, w2), b2))
+    t_result = sess.run([t_out2])
+    print('res', t_result)
 
-#variables_names =[v.name for v in tf.trainable_variables()]
-#values = sess.run(variables_names)
-#for k,v in zip(variables_names, values):
-#    print(k, v)
+    # variables_names =[v.name for v in tf.trainable_variables()]
+    # values = sess.run(variables_names)
+    # for k,v in zip(variables_names, values):
+    #     print(k, v)
