880e033afb604de636e8a16cbceda634001a59e9
Bernardo Vieira <bernardo5@live.com.pt>
2017-05-17 20:46:18 +0100

    round_results

--- test.py
+++ test.py
@@ -1,7 +1,18 @@
 import tensorflow as tf
+import numpy as np
 #
 import dataset
 import confusion
+
+# round'n'round
+def round_results(array_results):
+    #
+    matrix_result = np.zeros((len(array_results), len(array_results[0])), np.float)
+    for line in range(0, len(array_results)):
+        for column in range(0, len(array_results[0])):
+            matrix_result[line][column] = round(array_results[line][column])
+
+    return matrix_result
 
 def test_neural(w1, w2, b1, b2, sess):
     matrix_res = [[]]
@@ -23,7 +34,9 @@ def test_neural(w1, w2, b1, b2, sess):
     ]
 
     # debug stuff
-    print('result', t_result)
+    # print('result', t_result)
+
+    print(round_results(t_result[0]))
 
     # do the harlem shake
     # confusion.confusion_matrix_graphic(matrix_res)
