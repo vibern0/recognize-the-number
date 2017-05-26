65f87327cc56c8b8406d4010fc67a3a9b5276239
Bernardo Vieira <bernardo5@live.com.pt>
2017-05-26 22:31:56 +0100

    transform image and get array

--- transform/main.py
+++ transform/main.py
@@ -1,6 +1,16 @@
+from PIL import Image
 
-# Transform image
+col = Image.open("one.jpg")
+col = col.resize((26, 28), Image.ANTIALIAS)
+gray = col.convert('L')
+bw = gray.point(lambda x: 0 if x<128 else 255, '1')
+bw.save("result_bw.jpg")
+pixels = list(bw.getdata())
+width, height = bw.size
+pixels = [pixels[i * width:(i + 1) * width] for i in xrange(height)]
+pixels = [[1 if pixels[i][g] == 0 else 0 for g in xrange(width)] for i in xrange(height)]
 
-# recolor
-# resize
-# make a matrix of it
+for h in range(0, len(pixels)):
+	print(pixels[h])
+
+# test with trained neural network
