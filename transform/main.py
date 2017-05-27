f1e05dd408d1574d28c342a6c1e7056e0ff78584
Bernardo Vieira <bernardo5@live.com.pt>
2017-05-27 11:50:25 +0100

    organize

--- transform/main.py
+++ transform/main.py
@@ -1,14 +1,24 @@
 from PIL import Image
 
+# open image
 col = Image.open("one.jpg")
-col = col.resize((26, 28), Image.ANTIALIAS)
+
+# resize image
+col = col.resize((20, 16), Image.ANTIALIAS)
+
+# make it black and white
 gray = col.convert('L')
 bw = gray.point(lambda x: 0 if x<128 else 255, '1')
-bw.save("result_bw.jpg")
+
+# get transformed data
 pixels = list(bw.getdata())
 width, height = bw.size
+
+# make a matrix
 pixels = [pixels[i * width:(i + 1) * width] for i in xrange(height)]
-pixels = [[1 if pixels[i][g] == 0 else 0 for g in xrange(width)] for i in xrange(height)]
+
+# update values
+pixels = [[1. if pixels[i][g] == 0 else 0. for g in xrange(width)] for i in xrange(height)]
 
 for h in range(0, len(pixels)):
 	print(pixels[h])
