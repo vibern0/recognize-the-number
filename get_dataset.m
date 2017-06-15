049dd7cfdf7033cfd5693d876f826f136c0e870c
Bernardo Vieira <bernardo5@live.com.pt>
2017-06-15 17:57:48 +0100

    matlab code bug fix

--- get_dataset.m
+++ get_dataset.m
@@ -1,19 +1,31 @@
 load('/home/bernardovieira/Dropbox/Faculdade/3/s2/CR/Enunciado e Ficheiros complementares-20170506/binaryalphadigs.mat')
-[w, h] = size(dat);
+[h, w] = size(dat);
 fileID = fopen('generated_dataset.txt','w');
 fprintf(fileID, 'input = [\n');
-for a = 1:w
-    for b = 1:h
+for a = 1:h
+    for b = 1:w
         mn = dat{a, b};
-        fprintf(fileID, '\t[\n\t\t%.1f, ', mn(1));
-        for n = 2:319
-            if(mod(n, 16) == 0)
-                fprintf(fileID, '%.1f\n\t\t', mn(n));
-            else
-                fprintf(fileID, '%.1f, ', mn(n));
+        if(b == 1)
+            fprintf(fileID, '\t[\n');
+        end
+        
+        for l = 1:20
+            for c = 0:15
+                if(l == 1 && c == 0)
+                    fprintf(fileID, '\t\t[\n\t\t\t%.1f, ', mn(1));
+                elseif(l == 20 && c == 15)
+                    if(b == w)
+                        fprintf(fileID, '%.1f\n\t\t]\n\t],\n', mn(320));
+                    else
+                        fprintf(fileID, '%.1f\n\t\t],\n', mn(320));
+                    end
+                elseif(c == 15)
+                    fprintf(fileID, '%.1f,\n\t\t\t', mn(20 * c + l));
+                else
+                    fprintf(fileID, '%.1f, ', mn(20 * c + l));
+                end
             end
         end
-        fprintf(fileID, '%.1f\n\t],\n', mn(320));
     end
 end
 fprintf(fileID, ']');
